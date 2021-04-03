7b067eebcfa376a2da71e6c3afec3eb2c955c829
Connor Hamilton Inglis <chinglis@umich.edu>
2021-04-03 15:31:19 -0400

    Created basic counter app for reference

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Landing Page
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  buttonClick = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button name="testButton" onClick={(e) => this.buttonClick(e)}>
+            Click Me and Check the Console
+          </button>
+          <button name="testButton2" onClick={(e) => this.buttonClick(e)}>
+            Click Me and Check the Console
+          </button>
+          <p>Counter: {this.state.counter}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
