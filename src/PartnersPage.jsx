7d63debf6d00918e833f6ad1be692cabb700514f
Katherine Joy Knister <kknister@umich.edu>
2021-04-11 17:36:09 -0400

    Partners page work

--- src/PartnersPage.jsx
+++ src/PartnersPage.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './PartnersPage.css';
+import background from "./images/ssd_photo.jpg"
 
 class PartnersPage extends Component {
   constructor(props) {
@@ -9,7 +10,21 @@ class PartnersPage extends Component {
   render() {
     return (
     <div className="PartnersPage">
-        <p>Partners Page</p>
+        <div className="PartnerComponent">
+          <img className="PartnerImage" src="./images/ssd_photo.jpg"></img>
+          <p className="PartnerName">Services for Students with Disabilities</p>
+          <p className="PartnerDescription">Description will go here</p>
+        </div>
+        <div className="PartnerComponent">
+          <img className="PartnerImage" src="./images/ssd_photo.jpg"></img>
+          <p className="PartnerName">Services for Students with Disabilities</p>
+          <p className="PartnerDescription">Description will go here</p>
+        </div>
+        <div className="PartnerComponent">
+          <img className="PartnerImage" src="./images/ssd_photo.jpg"></img>
+          <p className="PartnerName">Services for Students with Disabilities</p>
+          <p className="PartnerDescription">Description will go here</p>
+        </div>
     </div>
     );
   }
