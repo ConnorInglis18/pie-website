d449d706313a8a75112adbe3804b59d2b7b4a6fa
Connor Hamilton Inglis <chinglis@umich.edu>
2021-04-12 17:30:47 -0400

    Created the Partner Cards

--- src/PartnersPage.jsx
+++ src/PartnersPage.jsx
@@ -1,20 +1,49 @@
 import { Component } from 'react';
 import './PartnersPage.css';
 import {partnersJSON} from "./backend/partnerInfoJSON";
+// import michiganmarchingband from "./UM-Marching-Band.png"
 
 class PartnersPage extends Component {
+  shortenedDescription = (name, description) => {
+    if (description.length < 120) {
+      return description + "..."
+    }
+    if (description.length < 220) {
+      return description.substring(0, description.length) + "..."
+    }
+    if (name.length < 25) {
+      return description.substring(0, 220) + "..."
+    } else {
+      return description.substring(0, 120) + "..."
+    }
+    
+  }
+
   render() {
     return (
       <div className="PartnersPage">
-        {partnersJSON.map(partner => {
-          return (
-            <div className="PartnerComponent">
-              <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
-              <p className="PartnerName">{partner.name}</p>
-              <p className="PartnerDescription">{partner.description}</p>
-              <a href={partner.websiteUrl}>View Website</a>
-            </div>
-          )
+        {partnersJSON.map((partner, index) => {
+          if (index % 2 === 0) {
+            return (
+              <button className="PartnerComponent Left">
+                <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
+                <div className="PartnerInfo">
+                  <p className="PartnerName">{partner.name}</p>
+                  <p className="PartnerDescription">{this.shortenedDescription(partner.name, partner.description)}</p>
+                </div>
+              </button>
+            )
+          } else {
+            return (
+              <button className="PartnerComponent Right">
+                <div className="PartnerInfo">
+                  <p className="PartnerName">{partner.name}</p>
+                  <p className="PartnerDescription">{this.shortenedDescription(partner.name, partner.description)}</p>
+                </div>
+                <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
+              </button>
+            )
+          }
         })}
       </div>
     );
--- src/backend/partnerInfoJSON.jsx
+++ src/backend/partnerInfoJSON.jsx
@@ -3,13 +3,19 @@ export const partnersJSON = [
     name: "Michigan Marching Band",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     websiteUrl: "https://www.michiganmarchingband.com/",
-    imgSrc: "https://arts.umich.edu/wp-content/uploads/sites/35/mc-image-cache/2015/12/marching_band.jpg"
+    imgSrc: "/images/UM-Marching-Band.png"
   },
   {
     name: "Michigan SSD",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     websiteUrl: "https://ssd.umich.edu/",
-    imgSrc: "https://ssd.umich.edu/files/ssd/styles/crop_640x480/public/ssd_mugs.jpg?itok=t_lzQJ_E"
+    imgSrc: "/images/SSD.jpg"
+  },
+  {
+    name: "University Of Michigan Adaptive Sports",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    websiteUrl: "https://ssd.umich.edu/",
+    imgSrc: "/images/UM-Adaptive-Sports.jpg"
   },
   {
     name: "Jane Doe",
