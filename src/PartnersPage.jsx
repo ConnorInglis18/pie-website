053c1a7b0402d3840b6e11840c540bc4e2ec319d
Connor Hamilton Inglis <chinglis@umich.edu>
2021-04-12 18:16:14 -0400

    Added Partner Page Functionality. The side panel does not look good yet but a panel opens on a partner click

--- src/PartnersPage.jsx
+++ src/PartnersPage.jsx
@@ -4,6 +4,14 @@ import {partnersJSON} from "./backend/partnerInfoJSON";
 // import michiganmarchingband from "./UM-Marching-Band.png"
 
 class PartnersPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSidePanel: false,
+      selectedIdx: 0
+    }
+  }
+  
   shortenedDescription = (name, description) => {
     if (description.length < 120) {
       return description + "..."
@@ -12,40 +20,69 @@ class PartnersPage extends Component {
       return description.substring(0, description.length) + "..."
     }
     if (name.length < 25) {
-      return description.substring(0, 220) + "..."
+      return description.substring(0, 210) + "..."
     } else {
-      return description.substring(0, 120) + "..."
+      return description.substring(0, 110) + "..."
     }
-    
+  }
+
+  showSidePanel = (e, index) => {
+    console.log("CLICKED " + index)
+    e.preventDefault();
+    this.setState({
+      showSidePanel: true,
+      selectedIdx: index
+    })
+  }
+
+  closePanel = (e) => {
+    e.preventDefault();
+    this.setState({
+      showSidePanel: false,
+    })
   }
 
   render() {
     return (
       <div className="PartnersPage">
-        {partnersJSON.map((partner, index) => {
-          if (index % 2 === 0) {
-            return (
-              <button className="PartnerComponent Left">
-                <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
-                <div className="PartnerInfo">
-                  <p className="PartnerName">{partner.name}</p>
-                  <p className="PartnerDescription">{this.shortenedDescription(partner.name, partner.description)}</p>
-                </div>
-              </button>
-            )
-          } else {
-            return (
-              <button className="PartnerComponent Right">
-                <div className="PartnerInfo">
-                  <p className="PartnerName">{partner.name}</p>
-                  <p className="PartnerDescription">{this.shortenedDescription(partner.name, partner.description)}</p>
-                </div>
-                <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
-              </button>
-            )
-          }
-        })}
+        <div className="PartnersPageList">
+          {partnersJSON.map((partner, index) => {
+            if (index % 2 === 0) {
+              return (
+                <button key={index} className="PartnerComponent Left" onClick={(e) => this.showSidePanel(e, index)}>
+                  <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
+                  <div className="PartnerInfo">
+                    <p className="PartnerName">{partner.name}</p>
+                    <p className="PartnerDescription">{this.shortenedDescription(partner.name, partner.description)}</p>
+                  </div>
+                </button>
+              )
+            } else {
+              return (
+                <button key={index} className="PartnerComponent Right" onClick={(e) => this.showSidePanel(e, index)}>
+                  <div className="PartnerInfo">
+                    <p className="PartnerName">{partner.name}</p>
+                    <p className="PartnerDescription">{this.shortenedDescription(partner.name, partner.description)}</p>
+                  </div>
+                  <img className="PartnerImage" src={partner.imgSrc} alt="PIE Partner" />
+                </button>
+              )
+            }
+          })}
+        </div>
+        {this.state.showSidePanel ?
+          <div id="PartnerSidePanel">
+            <button id="PartnerSideClose" onClick={this.closePanel}>X</button>
+            <img id="PartnerSideImage" src={partnersJSON[this.state.selectedIdx].imgSrc} alt="PIE Partner" />
+            <div id="PartnerSideInfo">
+              <p id="PartnerSideName">{partnersJSON[this.state.selectedIdx].name}</p>
+              <p id="PartnerSideDescription">{partnersJSON[this.state.selectedIdx].description}</p>
+              <button>View Website</button>
+            </div>
+          </div> : <div></div>
+        }
       </div>
+      
     );
   }
 }
