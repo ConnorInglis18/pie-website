6a1fd24b3d4b51b6c530c9fd49f16a97b3314093
Connor Hamilton Inglis <chinglis@umich.edu>
2021-04-16 15:45:19 -0400

    Updated PIE statement

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -9,7 +9,7 @@ class LandingPage extends Component {
   render() {
     return (
       <div className="LandingPage">
-          <p style={this.props.applyStylesFxn(48, "Nunito Sans")}>Welcome To Partners In Excellence, but you can call us PIE. PIE is a collaborative effort between disabled and non-disabled stakeholders from the Adaptive Sports & Fitness Program and the Office of Services for Students with Disabilities at the University of Michigan. Through the use of our growing number of Partners, we strive to improve Accessibility for everyone.</p>
+          <p style={this.props.applyStylesFxn(48, "Nunito Sans")}>Welcome To Partners In Excellence, but you can call us PIE. PIE is a collaborative effort between people who have any disability and supportive partners who strive to make their community inclusive for all. PIE was created by the Adaptive Sports & Fitness Program and the Office of Services for Students with Disabilities at the University of Michigan. Through the use of our growing number of Partners, we strive to improve Accessibility for everyone.</p>
           <button
             className="viewPartners"
             style={this.props.applyStylesFxn(24, "Nunito Sans")}
